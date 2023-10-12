5162c12af4b8ea69c6b24e7e0f21dd06d2ab4941
JulienVaglia <julien.vaglia@gmail.com>
2023-10-12 11:19:42 +0200

    refonte dossier

---
+++ dvdstorecart/src/test/java/com/simplon/dvdstorecart/DvdstorecartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplon.dvdstorecart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DvdstorecartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
